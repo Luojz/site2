0d345c35d856e988cdd4ee74e4812b60f9ababfd
czf2015 <122385930@qq.com>
2020-03-13 09:57:58 +0800

    solution 应用场景&方案架构结构

--- src/pages/solution/bussiness/TabsIndustry/index.js
+++ src/pages/solution/bussiness/TabsIndustry/index.js
@@ -6,12 +6,12 @@ import { protocol } from "../../../../config/apis";
 
 export default ({ data }) => (
   <div id="solution-tabsIndustry">
-    <h3>{data.title}</h3>
-    <ul>
+    <h3 className="solution-tabsIndustry-title">{data.title}</h3>
+    <ul className="solution-tabsIndustry-content">
       {data.tabs.map(({ title, iconUrl }, idx) => (
         <li key={title}>
-          <h4>{title}</h4>
-          <img src={`${protocol}${iconUrl}`} />
+          <h4 className="solution-tabsIndustry-content-title">{title}</h4>
+          <img className="solution-tabsIndustry-content-image" src={`${protocol}${iconUrl}`} />
           {adapter(data.children[idx]).map(({ id, type, Component, data }) => (
             <Component data={data} key={id || type} />
           ))}
