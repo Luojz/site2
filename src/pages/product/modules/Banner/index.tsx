68682c701ad62caf79547d7e907cecd4c336f1b3
zhaodagang <zhaodagang@datalliance.com.cn>
2020-03-16 18:51:41 +0800

    add read json data

--- src/pages/product/modules/Banner/index.tsx
+++ src/pages/product/modules/Banner/index.tsx
@@ -3,30 +3,34 @@ import mock from './mock.js';
 import './index.less';
 
 const Banner = ({data = mock}) => {
-    console.log(data)
+    console.log("banner data: ", data)
     return (
         <div className="c-hero-section J-pdBanner">
             <div className="c-hero-section-body">
                 <div className="c-hero-section-inner">
                     <div className="c-hero-section-ct">
                         <h1 className="c-hero-section-tit">
-                            云服务器
+                            {data.extra.banner.title}
                         </h1>
                         <div className="c-hero-section-des">
-                            <p>稳定、安全、弹性、高性能的云端计算服务，实时满足您的多样性业务需求</p>
+                            <p>{data.extra.banner.subTitle}</p>
                         </div>
                         <div className="c-hero-section-btn">
-                            <a href="//console.yun.ccb.com/cvm/index" className="c-btn buy">
-                                点击继续
+                            <a href={data.extra.banner.btnLink} className="c-btn buy">
+                                {data.extra.banner.btnTitle}
                             </a>
-                            <a href="//doc.yun.ccb.com/tcloud/Compute/CloudServer" className="c-btn p-weak-btn">帮助文档</a>
+                            {(data.extra.banner.addonButtons).length > 0 ?
+                                <a href={data.extra.banner.addonButtons[0].btnLink}
+                                   className="c-btn p-weak-btn">{data.extra.banner.addonButtons[0].btnTitle}</a>
+                                :''
+                            }
                         </div>
                     </div>
                 </div>
             </div>
 
             <div className="c-hero-icon-mod">
-                <img src="//imagecachexxfb.yun.ccb.com/static/product/product_banner.jpg" alt="" width="100%" height="100%"/>
+                <img src={data.extra.banner.imgUrl} width="100%" height="100%"/>
             </div>
 
         </div>
