483f9097a175c8cf82f7a884ee92de4632afe0a7
czf2015 <122385930@qq.com>
2020-03-19 00:20:42 +0800

    方案架构样式复原

--- src/pages/solution/bussiness/TabsFrameWork/partials/FrameWorkCloud/index.js
+++ src/pages/solution/bussiness/TabsFrameWork/partials/FrameWorkCloud/index.js
@@ -1,29 +1,21 @@
-import React, { useState, useRef, useEffect } from 'react'
+import React from 'react'
 import './index.less'
 import Icon from '@/components/Icon'
 import {protocol} from '@/config/apis'
 
 
-function List({ data }) {
-    const [fold, setFold] = useState(false)
-    console.log('222',data.list.length)
-    return (
+export default ({data}) => (
+    <div className="solution-tabsIndustry-frameWorkCloud">
+        <h3>{data.title}</h3>
         <ul>
-            {data.list.map(({iconUrl, title, moreUrl},idx) => (
-                <li key={idx} style={{ display: idx < 4 || fold ? 'block' : 'none' }}>
+            {data.list.map(({iconUrl, title, moreUrl}) => (
+                <li key={title}>
                     <a href={`${protocol}${moreUrl}`}>
-                        <img src={`${protocol}${iconUrl}`} />
+                        <Icon src={`${protocol}${iconUrl}`}></Icon>
                         <span>{title}</span>
                     </a>
                 </li>
             ))}
-            <span onClick={() => setFold(true)} style={{ display: data.list.length > 3 && !fold ? '' : 'none' }}>更多产品</span>
         </ul>
-    )
-}
-export default ({data}) => (
-    <div className="solution-tabsIndustry-frameWorkCloud">
-        <h3 className="title">{data.title}</h3>
-        <List data={data}/>
     </div>
 )
