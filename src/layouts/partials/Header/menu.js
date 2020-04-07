60b3f1ed680c223e3c72d485190ceb6d56a45620
zhaodagang <zhaodagang@datalliance.com.cn>
2020-04-07 18:49:47 +0800

    add new fiels

--- src/layouts/partials/Header/menu.js
+++ src/layouts/partials/Header/menu.js
@@ -70,7 +70,8 @@ class Menu extends Component {
                                                                 <div className="m-tit-level-3 J-qcMenuTitle" key={index3}>
                                                                     {
                                                                         item3.title !== '服务公告'?
-                                                                            <a href={item3.link.replace('//yun.ccb.com', '//localhost:3000')}>{item3.title}</a>
+
+                                                                            <a href={item3.link.replace('//yun.ccb.com', 'http://niezongxia.iask.in:58447')}>{item3.title}</a>
                                                                             :
                                                                             <a href={item3.link}>{item3.title}</a>
                                                                     }
